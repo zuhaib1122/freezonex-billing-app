54554a4ae9bfe04a2bd86f03528b06cf41b6c120
zuhaib1122 <zuhaib12325@gmail.com>
2026-08-29 09:50:01 +0500

    Refactor Google Sheets connection and update logic

--- customer_app.py
+++ customer_app.py
@@ -1,21 +1,15 @@
 import streamlit as st
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import datetime
+from google.oauth2.service_account import Credentials
 
-# --- 1. Connection Logic (UPDATED FOR CLOUD) ---
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
-# This line pulls the data from your Streamlit Secrets box
-creds_dict = st.secrets["gcp_service_account"]
-
-# This line uses the secret data instead of a local file
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+# --- 1. Connection Logic ---
+scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+creds = Credentials.from_service_account_file('frezonex-key.json', scopes=scope)
 client = gspread.authorize(creds)
 
-# Use the exact name of your sheet h    ere
+# Use the exact name of your sheet here
 sheet = client.open("freezonex_data").sheet1
-
 # --- 2. Initialize App Memory ---
 if 'page' not in st.session_state:
     st.session_state.page = 'input'
@@ -24,11 +18,13 @@ if 'page' not in st.session_state:
 if st.session_state.page == 'input':
     st.title("Customer Billing Portal")
     
+    # We put the form in a variable called 'my_form'
     with st.form("billing_form"):
         name = st.text_input("Customer Name")
         item = st.text_input("Product or Service")
         price = st.number_input("Price (PKR)", min_value=0)
         
+        # This is the button you see in your screenshot
         submit_button = st.form_submit_button("Generate Invoice")
 
     if submit_button:
@@ -37,11 +33,12 @@ if st.session_state.page == 'input':
                 current_date = datetime.date.today().strftime("%d-%m-%Y")
                 sheet.append_row([current_date, name, item, price])
                 
+                # Save data so we can show it on the next page
                 st.session_state.invoice_data = {
                     "date": current_date, "name": name, "item": item, "price": price
                 }
                 st.session_state.page = 'invoice'
-                st.rerun() 
+                st.rerun() # This flips the page to the Invoice
         else:
             st.error("Please fill in the Name and Product fields.")
 
@@ -60,5 +57,3 @@ elif st.session_state.page == 'invoice':
     if st.button("Add New Entry"):
         st.session_state.page = 'input'
         st.rerun()
-st.divider()
-st.caption(" Created by Hafiz Zuhaib Idrees")
